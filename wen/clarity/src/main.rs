cfeb8311b95fb7d72d11056a0c7f6b7ad1d98c3d
Wen Myers <Wmyers559@users.noreply.github.com>
2020-03-18 19:23:40 -0600

    Added pretty color printing:

--- wen/clarity/src/main.rs
+++ wen/clarity/src/main.rs
@@ -4,20 +4,35 @@
  * I,,,should update this comment to be actually useful at some point
  *
  *  - Wen
+ *
+ *
+ *  TODO:
+ *   * Finish engine
+ *     * Need some map struct
+ *     * Actions!
+ *     * Nice display functions for things
+ *     * ????
+ *   * Implement a nicer UI? Curses? (Cursive crate looks neat)
+ *   * Start writing the story
+ *   * Start making maps
+ *   * input loop?
  */
 
+extern crate ansi_term;
+
+use ansi_term::Color;
 
 mod engine;
 
 fn main() {
 
-    println!("Welcome to
+    println!("{}", Color::Purple.paint("Welcome to
 ┏━╸╻  ┏━┓┏━┓╻╺┳╸╻ ╻ 
 ┃  ┃  ┣━┫┣┳┛┃ ┃ ┗┳┛╹
 ┗━╸┗━╸╹ ╹╹┗╸╹ ╹  ╹ ╹ A text-based exploration
 
 
-");
+"));
 
 
     let mut play = engine::Player::new("Nausicaa".to_string(),
