0415e32e175de632c393775d9c1705dcbc37715a
Wen Myers <Wmyers559@users.noreply.github.com>
2020-03-19 13:58:59 -0600

    Added basis of input loop

--- wen/clarity/src/main.rs
+++ wen/clarity/src/main.rs
@@ -21,6 +21,8 @@
 extern crate ansi_term;
 
 use ansi_term::Color;
+use std::io;
+use std::io::Write;
 
 mod engine;
 
@@ -49,4 +51,24 @@ fn main() {
 
     println!("\n");
     play.display_inventory();
+
+
+    let mut command = String::new();
+
+    loop {
+        print!("> ");
+        io::stdout().flush();
+        io::stdin().read_line(&mut command).expect("Failed to read line");
+
+        // Debug
+        //println!("{} -> {}", command.escape_debug(), command.trim_end().escape_debug());
+        if command.trim_end().eq_ignore_ascii_case("exit") {
+            println!("Goodbye.");
+            break;
+        } else {
+            println!("I don't know how to do that");
+        }
+
+        command.clear();
+    }
 }
